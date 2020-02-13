1563b957d99f1a02602196162a20d62e6a2f9aab
Maaruf Dauda <mkdauda.work@gmail.com>
2020-02-13 16:35:55 +0100

    :sparkles: Complete items addition and removal

--- src/adv.py
+++ src/adv.py
@@ -5,7 +5,7 @@ from player import Player
 
 room = {
     'outside':  Room("Outside the Cave Entrance",
-                     "North of you, the cave mount beckons", 'outside', ['stick, stone']),
+                     "North of you, the cave mount beckons", 'outside', ['stick', 'stone']),
 
     'foyer':    Room("in the Foyer", """Dim light filters in from the south. Dusty
 passages run north and east.""", 'foyer', ['lamp']),
@@ -45,8 +45,8 @@ def startSession():
     currentPlayer = Player(heroName, room['outside'])
     
     firstMove = input(f"""\n\nYou may begin, {currentPlayer.name}. You are in {currentPlayer.room.name}.
-{currentPlayer.room.desc}. What shall your first move be?\n
-Your options are: "w" to advance, "s" to retreat, "a" to strafe left and "d" to strafe right. 
+{currentPlayer.room.desc}.  There are {len(currentPlayer.room.items)} items to add to inventory in this room. What shall your first move be?\n
+Your options are: "w" to advance, "s" to retreat, "a" to strafe left and "d" to strafe right. To add an item to inventory, hit o
 Decide now: """)
     maintainSession(currentPlayer, firstMove)
 # Write a loop that:
@@ -82,17 +82,40 @@ def maintainSession(player, firstMove):
     while (playing):
         if (moveIsCardinal(nextMove)):
             if (hasPath(player, nextMove)):
-                print(f"\nSuccess, you are {player.room.name}. {player.room.desc}")
+                print(f"\nSuccess, you are {player.room.name}. {player.room.desc}. There are {len(player.room.items)} items to add to inventory in this room.")
                 nextMove = input("\nWhat shall your next move be? ")
             else: 
                 nextMove = input("\nHow hard can this be? Make a valid move: ")
         else:
             if (nextMove.lower() == 'q'):
                 playing = False
+            elif (nextMove.lower() == 'i'):
+                print(f"\nPlayer {player.name} currently has the following items in their inventory:\n")
+                for item in player.items:
+                    print(f"Item: {item}")
+                nextMove = input("\n What shall your next move be? You may drop an item by hitting n: ")
+            elif (nextMove.lower() == 'o'):
+                print(f"\nThe items available for pick up here are:\n")
+                for item in enumerate(player.room.items):
+                    print(f"Item: {item}")
+                nextMove = input("\n What shall your next move be? To pick up an item, just type its name: ")
+                if (len(nextMove) > 1 ):
+                    nextMove = get_item(nextMove, player)
             else:
-                nextMove = input("\nFailure\n\nChoose one of the four cardinals, please w, s, a or d: ")
+                nextMove = input("\nFailure\n\nChoose one of the four cardinals, please w, s, a or d. i to see inventory and q to quit: ")
     
     print('\n\nThank you for playing this time.\n\n')
+
+def get_item (item_name, player):
+    if (item_name in player.room.items):
+        player.items.append(item_name)
+        print(f"\nGreedy fool, {item_name} has been added to your inventory. You now have {len(player.room.items)} items")
+        player.items.remove(item_name)
+        nextMove = input("\nNow, what shall you do next? ")
+        return nextMove
+    else:
+        nextMove = input("Invalid move, you turd! Try: again: ")
+        return nextMove
 
 
 startSession()
